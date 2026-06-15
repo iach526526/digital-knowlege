81ba6e87b8d6beb9ba8a53fafd2aefbbcffd7978
Each Chen <info@iach.cc>
2026-06-15 08:42:32 +0800

    feat: qr code in first page

---
+++ global-top.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+  <a href="https://slide.iach.cc/2026-spring-chinese-tech-literacy/">
+    <div v-if="$nav.currentPage === 1" class="fixed bottom-4 left-4">
+      <img src="/img/exported_qrcode_image.svg" class="w-[130px] h-[130px]" alt="簡報 QR" />
+      <span class="text-xs mt-1 font-bold">在瀏覽器查看簡報獲得超連結的最佳體驗</span>
+    </div>
+</a>
+  </div>
+</template>
